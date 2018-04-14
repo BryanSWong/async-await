b989c35d9f7d8c54bab44c8875d773f06fa6fa68
Bryan <Bwong727@gmail.com>
2018-04-14 03:40:11 -0400

    code updates.

--- currency-convert.js
+++ currency-convert.js
@@ -2,16 +2,30 @@
 
 const axios = require('axios');
 
-const getExhangeRate = (from, to) => {
-    return axios.get (`http://api.fixer.io/latest?base=${from}`).then((response) => {
-        return response.data.rates[to];
-    });
+const getExhangeRate = async (from, to) => {
+    try{
+        const response = await axios.get (`http://api.fixer.io/latest?base=${from}`);
+        const rate = response.data.rates[to];
+
+        if(rate){
+            return rate;
+        } else {
+            throw new Error();
+        }
+    } catch (e){
+        throw new Error(`Unable to get exchange rate for ${from} and ${to}`);
+    }
+
 };
 
-const getCountries = (currencyCode) => {
-    return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
+const getCountries = async (currencyCode) => {
+    try{
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
         return response.data.map((country) => country.name);
-    });
+    } catch(e){
+        throw new Error(`Unable to get countries that use ${currencyCode}.`);
+    }
+
 };
 
 const convertCurrency = (from, to, amount) => {
@@ -26,9 +40,26 @@ const convertCurrency = (from, to, amount) => {
     });
 };
 
-convertCurrency('USD', 'CAD', 100).then((status) => {
+
+
+const convertCurrencyAlt = async (from, to, amount) => {
+    const countries = await getCountries(to);
+    const rate = await getExhangeRate(from, to);
+
+    const exchangedAmount = amount * rate;
+
+    return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`;
+}
+
+convertCurrencyAlt('USD', 'EUR', 100).then((status) => {
     console.log(status);
+}).catch((e) => {
+    console.log(e.message);
 });
+
+// convertCurrency('USD', 'CAD', 100).then((status) => {
+//     console.log(status);
+// });
 
 // getExhangeRate('USD', 'EUR').then((rate) => {
 //     console.log(rate);
